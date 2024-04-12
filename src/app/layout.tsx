6f3e95ac4c60cc6315c30a74478b0a633ec718a3
troypoulter <troypoulter@gmail.com>
2024-04-12 22:24:49 +1000

    update metadata description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "shadcn/ui Progress bar max reproduction",
   description:
-    "A reproduction of the progress bar issue where the max isn't set correctly https://github.com/shadcn-ui/ui/issues/3082",
+    "A reproduction of the progress bar issue, where the max does not work if it isn't 100 - https://github.com/shadcn-ui/ui/issues/3082",
 };
 
 export default function RootLayout({
